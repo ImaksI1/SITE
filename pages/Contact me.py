a77407ae9f369b218b3433e1a8b8289f3abc95a6
ImaksI1 <121427111+ImaksI1@users.noreply.github.com>
2024-07-07 13:53:09 +0300

    update 2
    add selection topic

--- pages/Contact me.py
+++ pages/Contact me.py
@@ -1,20 +1,28 @@
 import streamlit as st
 from send_email import *
+import pandas
 
 st.header("Contact Me")
 
-with st.form(key="email_form"):
+with (st.form(key="email_form")):
     user_email = st.text_input("Your email adress")
-    message = st.text_area("Write your message")
+    topics = pandas.read_csv("topics.csv")
+    topic = st.selectbox("Select topic:", topics)
+    raw_message = st.text_area("Write your message")
     button = st.form_submit_button("Submit")
     if button:
-        SendEmail(f"{user_email}\n{message}")
+        message =f"""New message
+from: {user_email}
+topic: {topic}
+message: {raw_message}
+"""
+        SendEmail(message)
         st.info("Your email has been sent successfully")
         file = open("file.csv", "r")
         file_buffer = file.read()
         file.close()
         file = open("file.csv", "w")
-        file.write(f"{file_buffer}{user_email}, {message}\n")
+        file.write(f"{file_buffer}{message}")
         file.close()
 
 
--- send_email.py
+++ send_email.py
@@ -1,7 +1,7 @@
-import smtplib, ssl
+import smtplib, ssl, os
 
 sender_gmail = "imaksand123i@gmail.com"
-password = "rxvevqgjdnqptmch"
+password = os.getenv("password")
 receiver_gmail = "maksvijtovic98@gmail.com"
 host = "smtp.gmail.com"
 port = 465
@@ -9,8 +9,8 @@ context = ssl.create_default_context()
 server = smtplib.SMTP_SSL(host, port, context=context)
 
 
-def SendEmail(message):
+def SendEmail(email):
     server.login(sender_gmail, password)
-    server.sendmail(sender_gmail, receiver_gmail, message)
+    server.sendmail(sender_gmail, receiver_gmail, email)
 
 
